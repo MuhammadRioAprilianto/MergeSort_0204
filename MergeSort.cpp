03d8e559a8079009f850903c8dab3f921de513dd
MuhammadRioAprilianto <rma994636@gmail.com>
2025-04-17 10:55:00 +0700

    membuat fungsi input

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 
 int arr[20], B[20]; // membuat array utama dan array sementara
+int n; //menentukan input jumlah array
 
+void input(){
+    while (true){
+        
+    }
+}
 
